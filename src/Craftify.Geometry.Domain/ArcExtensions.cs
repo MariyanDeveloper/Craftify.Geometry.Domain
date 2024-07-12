b59feb286501423ce371f4e71ca07633593d6d42
MariyanSlesar <marianslesar.08@gmail.com>
2024-07-12 08:19:11 +0300

    Move logic to evaluate curves to extension methods

---
+++ src/Craftify.Geometry.Domain/ArcExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Craftify.Geometry.Domain;
+
+public static class ArcExtensions
+{
+    public static Point3D Evaluate(this Arc arc, double parameter)
+    {
+        var angle = arc.StartAngle + parameter * (arc.EndAngle - arc.StartAngle);
+        return new Point3D(
+            arc.Center.X + arc.Radius * Math.Cos(angle),
+            arc.Center.Y + arc.Radius * Math.Sin(angle),
+            arc.Center.Z
+        );
+    }
+}
--- src/Craftify.Geometry.Domain/Line.cs
+++ src/Craftify.Geometry.Domain/Line.cs
@@ -1,10 +1,3 @@
 ﻿namespace Craftify.Geometry.Domain;
 
-public record Line(Point3D Start, Point3D End) : Curve
-{
-    public Point3D Evaluate(double t) => new Point3D(
-        Start.X + t * (End.X - Start.X),
-        Start.Y + t * (End.Y - Start.Y),
-        Start.Z + t * (End.Z - Start.Z)
-    );
-}
+public record Line(Point3D Start, Point3D End) : Curve;
