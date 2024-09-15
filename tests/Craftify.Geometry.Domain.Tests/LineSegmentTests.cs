e4a0e5f36e87f8989cd89531cb12a47153f49d0f
MariyanSlesar <marianslesar.08@gmail.com>
2024-09-15 12:12:18 +0300

    Add GetPointAtParameter tests for LineSegment

---
+++ tests/Craftify.Geometry.Domain.Tests/LineSegmentTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+
+namespace Craftify.Geometry.Domain.Tests;
+
+public class LineSegmentTests
+{
+    [Theory]
+    [MemberData(nameof(GetPointAtParameterTestData))]
+    public void GetPointAtParameter_ShouldReturnExpectedPointAtGivenParameter(double parameter, Point3D expectedPoint)
+    {
+        var line = Line.ByStartPointAndEndPoint(
+            Point.ByCoordinates(2, 0),
+            Point.ByCoordinates(10, 0)
+        );
+        var pointAtParameter = line.GetPointAtParameter(parameter);
+
+        pointAtParameter.Should().Be(expectedPoint);
+    }
+
+    public static IEnumerable<object[]> GetPointAtParameterTestData()
+    {
+        yield return
+        [
+            0,
+            Point.ByCoordinates(2)
+        ];
+        yield return
+        [
+            0.5,
+            Point.ByCoordinates(6)
+        ];
+        yield return
+        [
+            1,
+            Point.ByCoordinates(10)
+        ];
+    }
+}
