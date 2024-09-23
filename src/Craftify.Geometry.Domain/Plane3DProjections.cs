d04ad81eea801f71e87e2a385f2f62bad85335ed
maksym-Libovych <maksym.libovych@hotcode.lv>
2024-09-23 12:02:54 +0300

    feat(rendering): add point projection onto a plane

---
+++ src/Craftify.Geometry.Domain/Plane3DProjections.cs
@@ -0,0 +1,16 @@
+﻿namespace Craftify.Geometry.Domain;
+
+public static class Plane3DProjections
+{
+    public static Point3D ProjectPoint(this Plane3D plane, Point3D point)
+    {
+        var pointVector = Vector.ByCoordinates(point.X, point.Y, point.Z);
+        var planeOriginVector = Vector.ByCoordinates(plane.Origin.X, plane.Origin.Y, plane.Origin.Z);
+
+        Vector3D originToPoint = pointVector.Subtract(planeOriginVector);
+        double distanceFromPointToPlane = originToPoint.DotProduct(plane.Normal) / plane.Normal.DotProduct(plane.Normal);
+
+        Vector3D projectionVector = plane.Normal.Multiply(distanceFromPointToPlane);
+        return point.Translate(projectionVector.Negate());
+    }
+}
