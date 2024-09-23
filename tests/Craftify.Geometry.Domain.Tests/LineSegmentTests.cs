6c70fb1aba42d0657f1a8c073e124bc05ad3f8cc
maksym-Libovych <maksym.libovych@hotcode.lv>
2024-09-23 14:12:05 +0300

    test(intersectsTests): testing lines intersection

--- tests/Craftify.Geometry.Domain.Tests/LineSegmentTests.cs
+++ tests/Craftify.Geometry.Domain.Tests/LineSegmentTests.cs
@@ -26,4 +26,45 @@ public class LineSegmentTests
         yield return [0.5, Point.ByCoordinates(6)];
         yield return [1, Point.ByCoordinates(10)];
     }
+
+    [Theory]
+    [MemberData(nameof(IntersectsTestData))]
+    public void Intersects_ShouldReturnCorrectIntersectionPoint(
+        LineSegment mainLine,
+        LineSegment otherLine,
+        bool expectedIntersected,
+        Point3D? expectedIntersectionPoint)
+    {
+        bool actualIntersected = mainLine.Intersects(otherLine, out Point3D? actualIntersectionPoint);
+
+        actualIntersected.Should().Be(expectedIntersected);
+        actualIntersectionPoint.Should().Be(expectedIntersectionPoint);
+    }
+
+    public static IEnumerable<object[]> IntersectsTestData()
+    {
+        yield return
+        [
+            new LineSegment(new Point3D(0, 0, 0), new Point3D(1, 1, 0)),
+            new LineSegment(new Point3D(0, 1, 0), new Point3D(1, 0, 0)),
+            true,
+            new Point3D(0.5, 0.5, 0)
+        ];
+
+        yield return
+        [
+            new LineSegment(new Point3D(0, 0, 0), new Point3D(1, 0, 0)),
+            new LineSegment(new Point3D(0, 1, 0), new Point3D(1, 1, 0)),
+            false,
+            null
+        ];
+
+        yield return
+        [
+            new LineSegment(new Point3D(0, 0, 0), new Point3D(1, 1, 0)),
+            new LineSegment(new Point3D(2, 2, 0), new Point3D(3, 3, 0)),
+            false,
+            null
+        ];
+    }
 }
