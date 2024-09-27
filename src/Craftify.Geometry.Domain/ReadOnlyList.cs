801e6ee6a5d41892ad77cb83fcee6fb07427c917
MariyanSlesar <marianslesar.08@gmail.com>
2024-09-27 08:08:45 +0300

    feat(collections): add factory methods for readonly lists

---
+++ src/Craftify.Geometry.Domain/ReadOnlyList.cs
@@ -0,0 +1,11 @@
+﻿namespace Craftify.Geometry.Domain;
+
+public static class ReadOnlyList
+{
+    public static IReadOnlyList<T> Of<T>(T value) => [value];
+
+    public static IReadOnlyList<T> Of<T>(IEnumerable<T> values) => [.. values];
+
+    public static IReadOnlyList<T> Of<T>(IEnumerable<T> range1, IEnumerable<T> range2) =>
+        [.. range1, .. range2];
+}
