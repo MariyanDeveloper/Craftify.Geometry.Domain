897c8bd58d18b6549796939be4f479519f90149e
maksym-Libovych <maksym.libovych@hotcode.lv>
2024-09-27 17:37:09 +0300

    feat(dijkstra): add Dijkstra's algorithm

---
+++ src/Craftify.Geometry.Domain/Graph.cs
@@ -0,0 +1,5 @@
+﻿namespace Craftify.Geometry.Domain;
+
+#pragma warning disable S2094
+public abstract record Graph;
+#pragma warning restore S2094
---
+++ src/Craftify.Geometry.Domain/XYZAlmostEqualityComparer.cs
@@ -0,0 +1,30 @@
+﻿namespace Craftify.Geometry.Domain;
+
+#pragma warning disable S101
+public class XYZAlmostEqualityComparer(double tolerance = Defaults.Tolerance) : IEqualityComparer<IXYZ>
+#pragma warning restore S101
+{
+    private const int InitialSeedValue = 17;
+    private const int PrimeMultiplier = 23;
+
+    private readonly double _tolerance = tolerance;
+
+    public bool Equals(IXYZ x, IXYZ y)
+    {
+        return x.AlmostEqualTo(y, _tolerance);
+    }
+
+    public int GetHashCode(IXYZ xyz)
+    {
+        int xHash = xyz.X.RoundToTolerance(_tolerance).GetHashCode();
+        int yHash = xyz.Y.RoundToTolerance(_tolerance).GetHashCode();
+        int zHash = xyz.Z.RoundToTolerance(_tolerance).GetHashCode();
+
+        int hash = InitialSeedValue;
+        hash = hash * PrimeMultiplier + xHash;
+        hash = hash * PrimeMultiplier + yHash;
+        hash = hash * PrimeMultiplier + zHash;
+
+        return hash;
+    }
+}
