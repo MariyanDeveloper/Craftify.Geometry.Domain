6697e922477f459536d760bd727fe117f370a734
MariyanSlesar <marianslesar.08@gmail.com>
2024-09-29 14:22:05 +0300

    feat: add OfSequence factory method for ReadOnlyList

--- src/Craftify.Geometry.Domain/ReadOnlyList.cs
+++ src/Craftify.Geometry.Domain/ReadOnlyList.cs
@@ -8,4 +8,6 @@ public static class ReadOnlyList
 
     public static IReadOnlyList<T> Of<T>(IEnumerable<T> range1, IEnumerable<T> range2) =>
         [.. range1, .. range2];
+
+    public static IReadOnlyList<T> OfSequence<T>(params T[] elements) => elements;
 }
