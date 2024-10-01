c88b700cbc58c773cb4cb86de818eb996330c103
maksym-Libovych <maksym.libovych@hotcode.lv>
2024-10-01 19:04:50 +0300

    tests(dijkstra): find shortest path tests enhancements

---
+++ tests/Craftify.Geometry.Domain.Tests/CurveGraphTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+
+namespace Craftify.Geometry.Domain.Tests;
+
+public class CurveGraphTests
+{
+    [Theory]
+    [MemberData(nameof(DistinctNodesTestData))]
+    public void DistinctNodes_ShouldDistinctNodes(
+        CurveGraph graph,
+        IReadOnlyList<Point3D> expected)
+    {
+        graph.DistinctNodes().Should().IntersectWith(expected);
+    }
+
+    public static IEnumerable<object[]> DistinctNodesTestData()
+    {
+        var graph = new CurveGraph(ReadOnlyList.Of(
+        [
+            Line.ByStartPointAndEndPoint(
+                Point.ByCoordinates(0.0000000001, 0.0000000001, 0.0000000001),
+                Point.ByCoordinates(1, 1, 1)),
+
+            Line.ByStartPointAndEndPoint(
+                Point.ByCoordinates(0.0000000002, 0.0000000002, 0.0000000002),
+                Point.ByCoordinates(1, 1, 1)),
+
+            Line.ByStartPointAndEndPoint(
+                Point.ByCoordinates(0.0000000003, 0.0000000003, 0.0000000003),
+                Point.ByCoordinates(1, 1, 1)),
+
+            Line.ByStartPointAndEndPoint(
+                Point.ByCoordinates(1.0000000001, 1.0000000001, 1.0000000001),
+                Point.ByCoordinates(2, 2, 2)),
+
+            Line.ByStartPointAndEndPoint(
+                Point.ByCoordinates(1.0000000002, 1.0000000002, 1.0000000002),
+                Point.ByCoordinates(2, 2, 2))
+            ]
+        ));
+
+        yield return [
+            graph,
+            ReadOnlyList.Of(
+                [
+                    Point.ByCoordinates(0.0000000001, 0.0000000001, 0.0000000001),
+                    Point.ByCoordinates(1, 1, 1),
+                    Point.ByCoordinates(1.0000000001, 1.0000000001, 1.0000000001),
+                    Point.ByCoordinates(2, 2, 2),
+                ]
+            )
+        ];
+    }
+}
