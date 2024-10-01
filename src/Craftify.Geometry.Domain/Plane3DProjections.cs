e16cd8c247d8e3a2a1236426bb81668401b006fc
maksym-Libovych <maksym.libovych@hotcode.lv>
2024-10-01 18:05:48 +0300

    refactor(projection): refactoring point projection

--- src/Craftify.Geometry.Domain/Plane3DProjections.cs
+++ src/Craftify.Geometry.Domain/Plane3DProjections.cs
@@ -4,13 +4,9 @@ public static class Plane3DProjections
 {
     public static Point3D ProjectPoint(this Plane3D plane, Point3D point)
     {
-        var pointVector = Vector.ByCoordinates(point.X, point.Y, point.Z);
-        var planeOriginVector = Vector.ByCoordinates(plane.Origin.X, plane.Origin.Y, plane.Origin.Z);
-
-        Vector3D originToPoint = pointVector.Subtract(planeOriginVector);
-        double distanceFromPointToPlane = originToPoint.DotProduct(plane.Normal) / plane.Normal.DotProduct(plane.Normal);
-
-        Vector3D projectionVector = plane.Normal.Multiply(distanceFromPointToPlane);
-        return point.Translate(projectionVector.Negate());
+        var distanceFromPointToPlane = point.MeasureSignedDistanceToPointAlongVector(
+            plane.Origin, plane.Normal);
+        var projectionVector = plane.Normal.Normalize().Multiply(distanceFromPointToPlane);
+        return point.Translate(projectionVector);
     }
 }
